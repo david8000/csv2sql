11f6694e6ca969dfb86bbdcca4c17386f1ffe2db
david8000 <84601962+david8000@users.noreply.github.com>
2023-09-20 00:29:01 +0200

    Tabindex. Version 1.01 (?)

--- csv2sql/Form1.Designer.cs
+++ csv2sql/Form1.Designer.cs
@@ -82,7 +82,7 @@ namespace csv2sql
             this.textBox1.Location = new System.Drawing.Point(55, 30);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(663, 22);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.TabIndex = 10;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
@@ -101,7 +101,7 @@ namespace csv2sql
             this.textBox2.Location = new System.Drawing.Point(55, 294);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(663, 22);
-            this.textBox2.TabIndex = 3;
+            this.textBox2.TabIndex = 100;
             this.textBox2.Text = "nvarchar(100)";
             // 
             // dataGridView1
@@ -130,7 +130,7 @@ namespace csv2sql
             this.button1.Location = new System.Drawing.Point(729, 463);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(86, 31);
-            this.button1.TabIndex = 6;
+            this.button1.TabIndex = 120;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -141,7 +141,7 @@ namespace csv2sql
             this.button2.Location = new System.Drawing.Point(729, 425);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(86, 31);
-            this.button2.TabIndex = 7;
+            this.button2.TabIndex = 110;
             this.button2.Text = "Load";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -152,7 +152,7 @@ namespace csv2sql
             this.button3.Location = new System.Drawing.Point(166, 794);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(434, 37);
-            this.button3.TabIndex = 8;
+            this.button3.TabIndex = 130;
             this.button3.Text = "Start";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -163,7 +163,7 @@ namespace csv2sql
             this.textBox3.Location = new System.Drawing.Point(55, 94);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(663, 22);
-            this.textBox3.TabIndex = 10;
+            this.textBox3.TabIndex = 30;
             this.textBox3.Text = "c:\\example.csv";
             // 
             // label4
@@ -182,7 +182,7 @@ namespace csv2sql
             this.button4.Location = new System.Drawing.Point(729, 90);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(86, 31);
-            this.button4.TabIndex = 11;
+            this.button4.TabIndex = 40;
             this.button4.Text = "Choose";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
@@ -209,7 +209,7 @@ namespace csv2sql
             0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(137, 22);
-            this.numericUpDown1.TabIndex = 13;
+            this.numericUpDown1.TabIndex = 80;
             this.numericUpDown1.Value = new decimal(new int[] {
             100,
             0,
@@ -223,7 +223,7 @@ namespace csv2sql
             this.button5.Location = new System.Drawing.Point(729, 24);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(86, 31);
-            this.button5.TabIndex = 14;
+            this.button5.TabIndex = 20;
             this.button5.Text = "Choose";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
@@ -248,7 +248,7 @@ namespace csv2sql
             this.checkBox1.Location = new System.Drawing.Point(393, 161);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(15, 14);
-            this.checkBox1.TabIndex = 17;
+            this.checkBox1.TabIndex = 70;
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // label6
@@ -268,7 +268,7 @@ namespace csv2sql
             this.textBox4.MaxLength = 1;
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(41, 22);
-            this.textBox4.TabIndex = 19;
+            this.textBox4.TabIndex = 60;
             this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label7
@@ -288,7 +288,7 @@ namespace csv2sql
             this.textBox5.MaxLength = 2;
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(41, 22);
-            this.textBox5.TabIndex = 21;
+            this.textBox5.TabIndex = 50;
             this.textBox5.Text = ";";
             this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -321,7 +321,7 @@ namespace csv2sql
             this.checkBox2.Location = new System.Drawing.Point(66, 230);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(15, 14);
-            this.checkBox2.TabIndex = 24;
+            this.checkBox2.TabIndex = 90;
             this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // Form1
